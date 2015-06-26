2b412acf9bd6d2e8b1f15bf2f51a772ce4cff3ff
Fumiya Endo <enfumidayo@gmail.com>
2015-06-26 14:18:23 +0900

    概要詳細（企画の内容を閲覧する）機能を実装しました

---
+++ UKA.system/src/main/java/hal/tokyo/beans/IdeaDetailViewBean.java
@@ -0,0 +1,85 @@
+package hal.tokyo.beans;
+
+/**
+ *
+ * 企画内容閲覧画面で出力するデータを格納しています<br>
+ * postings、posting_contents、users、tesmsテーブルを結合しています
+ *
+ * @author enfumidayo
+ *
+ */
+public class IdeaDetailViewBean {
+	private String title;
+	private String date;
+	private String posting_content;
+	private String image;
+	private String name;
+	private String department_name;
+	private String profilecomment;
+	private int timelimit;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getPosting_content() {
+		return posting_content;
+	}
+
+	public void setPosting_content(String posting_content) {
+		this.posting_content = posting_content;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+
+	public String getProfilecomment() {
+		return profilecomment;
+	}
+
+	public void setProfilecomment(String profilecomment) {
+		this.profilecomment = profilecomment;
+	}
+
+	public int getTimelimit() {
+		return timelimit;
+	}
+
+	public void setTimelimit(int timelimit) {
+		this.timelimit = timelimit;
+	}
+
+}
---
+++ UKA.system/src/main/java/hal/tokyo/dao/IdeaDetailViewDao.java
@@ -0,0 +1,95 @@
+package hal.tokyo.dao;
+
+import hal.tokyo.beans.IdeaDetailViewBean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author enfumidayo
+ *
+ */
+public class IdeaDetailViewDao {
+	private Connection connection;
+	public PreparedStatement preparedStatement;
+
+	/**
+	 *
+	 * @throws NamingException
+	 * @throws SQLException
+	 */
+	public IdeaDetailViewDao() throws NamingException, SQLException {
+		InitialContext initialContext = new InitialContext();
+		DataSource dataSource = (DataSource) initialContext
+				.lookup("java:comp/env/jdbc/ukasystem");
+		this.connection = dataSource.getConnection();
+		this.connection.setAutoCommit(false);
+	}
+
+	/**
+	 *
+	 * @param connection
+	 * @throws NamingException
+	 * @throws SQLException
+	 */
+	public IdeaDetailViewDao(Connection connection) throws NamingException,
+			SQLException {
+		this.connection = connection;
+	}
+
+	/**
+	 *
+	 * @param postId
+	 * @return
+	 * @throws SQLException
+	 */
+	public IdeaDetailViewBean read(int postId) throws SQLException {
+		IdeaDetailViewBean ideaDetailViewBean = new IdeaDetailViewBean();
+		// TODO:円グラフ表示用のパーセンテージ出力処理を実装していません Fumiya Endo 150626
+		this.preparedStatement = this.connection
+				.prepareStatement("SELECT p.title, p.date, c.posting_content, u.image, u.name, d.department_name, u.profilecomment, DATEDIFF( DATE_ADD( (p.date), INTERVAL( SELECT t.terms_period FROM postings p, terms t WHERE t.terms_id = p.terms_id AND p.posting_id = 3 ) DAY), CURRENT_DATE() ) AS timelimit FROM postings p, posting_content c, users u, departments d WHERE p.posting_content_id = c.posting_content_id AND p.user_id = u.mailaddress AND u.department_id = d.department_id AND p.posting_id = ?");
+		this.preparedStatement.setInt(1, postId);
+		ResultSet resultSet = this.preparedStatement.executeQuery();
+		resultSet.next();
+		ideaDetailViewBean.setTitle(resultSet.getString("title"));
+		ideaDetailViewBean.setDate(resultSet.getString("date"));
+		ideaDetailViewBean.setPosting_content(resultSet
+				.getString("posting_content"));
+		ideaDetailViewBean.setImage(resultSet.getString("image"));
+		ideaDetailViewBean.setName(resultSet.getString("name"));
+		ideaDetailViewBean.setDepartment_name(resultSet
+				.getString("department_name"));
+		ideaDetailViewBean.setProfilecomment(resultSet
+				.getString("profilecomment"));
+		ideaDetailViewBean.setTimelimit(resultSet.getInt("timelimit"));
+		return ideaDetailViewBean;
+	}
+
+	/**
+	 *
+	 * @throws SQLException
+	 */
+	public void commit() throws SQLException {
+		this.connection.commit();
+	}
+
+	/**
+	 *
+	 * @throws SQLException
+	 */
+	public void rollback() throws SQLException {
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			this.connection = null;
+			throw e;
+		}
+	}
+}
