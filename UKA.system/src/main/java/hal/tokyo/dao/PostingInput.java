5c4b79f2f47ef42bfbb955ca5af54a504dc11e6b
need25 <need.not.neet@gmail.com>
2015-09-08 14:09:37 +0900

    プロジェクト投稿変更

--- UKA.system/src/main/java/hal/tokyo/dao/PostingInput.java
+++ UKA.system/src/main/java/hal/tokyo/dao/PostingInput.java
@@ -1,7 +1,6 @@
 package hal.tokyo.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -22,21 +21,21 @@ public class PostingInput {
                 .lookup("java:comp/env/jdbc/ukasystem");
         con = ds.getConnection();
         } catch (NamingException e) {
-        	e.printStackTrace();        
+        	e.printStackTrace();
         	} catch (SQLException e) {
-        		e.printStackTrace();        
-        		} 
+        		e.printStackTrace();
+        		}
 	}
 
 	public String selectCotent(int id) {
 		String content = "";
-		
+
 		// ステートメント生成
     	Statement stmt;
 		try {
 			stmt = con.createStatement();
-		
-	
+
+
         // SQLを実行
         String sqlStr = "SELECT `POSTING_CONTENT_ID`, `POSTING_CONTENT` FROM `posting_content` WHERE `POSTING_CONTENT_ID` ="+id;
         ResultSet rs = stmt.executeQuery(sqlStr);
@@ -49,30 +48,30 @@ public class PostingInput {
             //表示
             System.out.println("content："+content);
         }
-        
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return content;
 	}
-	
+
 	public int insertSummary(String userId ,String htmlText ,String title ,int postingContentId ,int postingTypeId,int termId) {
 		String msg = "";
         int id = 0;
         try {
 	        // ステートメント生成
         	Statement stmt = con.createStatement();
-        	
+
             String postConIns = "INSERT INTO posting_content(POSTING_CONTENT) VALUES ('"+htmlText+"')";
             System.out.println(postConIns);
         	stmt.executeUpdate(postConIns);
         	con.commit();
-        	
+
             // SQLを実行
             String sqlStr = "SELECT MAX(POSTING_CONTENT_ID) FROM posting_content";
             ResultSet rs = stmt.executeQuery(sqlStr);
-            
+
             // 結果行をループ
             while(rs.next()){
                 // レコードの値
@@ -81,10 +80,10 @@ public class PostingInput {
                 //表示
                 System.out.println("ID："+id);
             }
-            
-            String insPostings = "INSERT INTO postings(USER_ID, POSSESSION_VOTE, POSTING_TYPE_ID, TERMS_ID, STATUS, TITLE, POSTING_CONTENT_ID) VALUES ('"+userId+"',0,"+postingTypeId+","+termId+",1,'"+title+"',"+id+")";
+
+            String insPostings = "INSERT INTO postings(MAILADDRESS, POSSESSION_VOTE, POSTING_TYPE_ID, TERMS_ID, STATUS, TITLE, POSTING_CONTENT_ID) VALUES ('"+userId+"',0,"+postingTypeId+","+termId+",1,'"+title+"',"+id+")";
             System.out.println(insPostings);
-            
+
             try {
                 stmt.executeUpdate(insPostings);
                 con.commit();
@@ -92,11 +91,11 @@ public class PostingInput {
 				// TODO: handle exception
 				e.printStackTrace();
 			}
-            
+
             // 接続を閉じる
             rs.close();
             stmt.close();
-            
+
         }catch (Exception e){
             msg = "ドライバのロードに失敗しました";
             System.out.println(msg);
@@ -104,13 +103,13 @@ public class PostingInput {
         }
 		return id;
 	}
-	
+
 	public boolean insertDetail(String userId ,String htmlText ,String title ,int postingContentId ,int postingTypeId,int termId) {
 		String msg = "";
         try {
 	        // ステートメント生成
         	Statement stmt = con.createStatement();
-        	
+
         	//INSERT INTO product_content(POSTING_CONTENT) VALUES ('"+htmlText+"')
 
             String postConIns = "INSERT INTO posting_content(POSTING_CONTENT) VALUES ('"+htmlText+"')";
@@ -129,23 +128,23 @@ public class PostingInput {
                 //表示
                 System.out.println("ID："+id);
             }
-            
+
 	        //INSERT INTO postings(USER_ID, POSSESSION_VOTE, POSTING_TYPE_ID, TERMS_ID, STATUS, TITLE, POSTING_CONTENT_ID) VALUES ('"+userId+"',0,"+postingTypeId+","+termId+",1,'"+title+"',"+postingContentId+")
-        
+
             String insPostings = "INSERT INTO postings(USER_ID, POSSESSION_VOTE, POSTING_TYPE_ID, TERMS_ID, STATUS, TITLE, POSTING_CONTENT_ID) VALUES ('"+userId+"',0,"+postingTypeId+","+termId+",1,'"+title+"',"+id+")";
             stmt.executeUpdate(insPostings);
             con.commit();
             // 接続を閉じる
             rs.close();
             stmt.close();
-            
+
         }catch (Exception e){
             msg = "ドライバのロードに失敗しました";
             System.out.println(msg);
         }
 		return true;
 	}
-	
+
 	public void clos() {
         try {
 			con.close();
